4bc5285c68695ea6fef3511b245549ef1af15740
Mohnish Shah <shahmohnish@gmail.com>
2017-07-26 17:14:40 +0000

    update the files

--- tbay.py
+++ tbay.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Table, Column, Integer, String, DateTime, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
@@ -12,14 +13,6 @@ session = Session()
 Base = declarative_base()
 
 
-class Item(Base):
-    __tablename__ = "items"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    description = Column(String)
-    start_time = Column(DateTime, default=datetime.utcnow)
-
 class User(Base):
     __tablename__ = "users"
     
@@ -27,10 +20,42 @@ class User(Base):
     username = Column(String, nullable = False)
     password = Column(String, nullable = False)
     
-class bid(Base):
+    auction_items = relationship("Item", backref = "seller")
+    bids = relationship("Bid", backref = "bidder")
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    start_time = Column(DateTime, default=datetime.utcnow)
+    
+    seller_id = Column(Integer, ForeignKey("User.id"), nullable = False)
+    bids = relationship("Bid", backref = "item")
+    
+class Bid(Base):
     __tablename__ = "bids"
     
     id = Column(Integer, primary_key = True)
     price = Column(String, nullable = False)
 
-Base.metadata.create_all(engine)
+    user_id = Column(Integer, ForeignKey("User.id"), nullable=False)
+    item_id = Column(Integer, ForeignKey("Item.id"), nullable=False)
+
+def main():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    
+    Harry = User(username="Harry", password="password1")
+    David = User(username="David",password="password2")
+    Warren = User(username="Warren",password="password3")
+    
+    baseball = Item(name = "baseball", description = "Signed by Madison Bumgarner", seller = Harry)
+    
+    david_bid = Bid(price = 250, user = David, item = baseball)
+    warren_bid = Bid(price = 500, user = Warren, item = baseball)  
+
+    session.add_all()
+    session.commit()
+    
